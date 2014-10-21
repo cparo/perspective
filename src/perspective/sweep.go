1d156fa655d15fc424ba007d7a8689201f404c60
Christian Paro <cparo@digitalocean.com>
2014-10-21 05:34:52 -0400

    Add to sweep visualization commentary
    Human-friendly explanations of what's going on in the actual
    visualisation-drawing portion of the code.

--- src/perspective/sweep.go
+++ src/perspective/sweep.go
@@ -37,21 +37,39 @@ func NewSweep(
 
 // Record accepts an EventDataPoint and plots it onto the visualization.
 func (v *sweep) Record(e EventDataPoint) {
+
 	tMin := float64(e.Start)
 	tMax := float64(e.Start + e.Run)
 	y := v.h / 2
+
+	// Each event is drawn as an arc tracing its time of existance, with the
+	// x-axis representing absolute time and the y-axis being a logarithmic
+	// representation of time elapsed since the event was started. Since
+	// recorded events may collide in space with other recorded events in the
+	// visualization, we use a color progression to indicate the density of
+	// events in a given pixel of the visualization. This requires that we take
+	// into account the existing color of the point on the canvas to which the
+	// event will be plotted and calculate its new color as a function of its
+	// existing color.
 	for t := tMin; t <= tMax; t++ {
 		x := int(float64(v.w) * (t - v.tA) / (v.tΩ - v.tA))
 		yMin := v.h/2 - int(v.yLog2*(math.Log2(math.Max(1, t-tMin))))
 		for yʹ := y; yʹ > yMin; yʹ-- {
 			y = yʹ
 			if e.Status == 0 {
+				// Successes are plotted above the center line and allowed to
+				// desaturate in high-density regions for reasons of aesthetics
+				// and additional expressive range.
 				r16, g16, b16, _ := v.vis.At(x, y).RGBA()
 				r16 = uint32(math.Min(maxC16, float64(r16)+maxC16/v.colors/4))
 				g16 = uint32(math.Min(maxC16, float64(g16)+maxC16/v.colors/4))
 				b16 = uint32(math.Min(maxC16, float64(b16)+maxC16/v.colors))
 				plot(v.vis, x, y, r16, g16, b16)
 			} else {
+				// Failures are plotted below the center line and kept saturated
+				// to make them more visible and for the perceptual advantage of
+				// keeping them all red, all the time to clearly convey that
+				// they are an indication of something gone wrong.
 				r16, g16, b16, _ := v.vis.At(x, v.h-y).RGBA()
 				r16 = uint32(math.Min(maxC16, float64(r16)+maxC16/v.colors))
 				plot(v.vis, x, v.h-y, r16, g16, b16)
