49f79061cab8e76fa8bbc16b22e9f0e8bb67ae7b
Christian Paro <cparo@digitalocean.com>
2014-10-22 03:26:56 -0400

    Continued refactor of main for minimalism
    ...dropping more LOC, and reducing the amount of visual jumping-around
    a user would have to do to follow what is happening in the main method.

--- perspective_main.go
+++ perspective_main.go
@@ -9,73 +9,20 @@ import (
 	"time"
 )
 
-// Variables for command-line option flags.
 var (
 	errorClassConf string  // Optional conf file for error classification.
-	typeFilter     int     // Event type code to filter for, if non-negative.
+	typeFilter     int     // Event type to filter for, if non-negative.
 	tA             int     // Lower limit of time range to be visualized.
 	tΩ             int     // Upper limit of time range to be visualized.
 	xGrid          int     // Number of horizontal grid divisions.
 	yLog2          float64 // Number of pixels over which elapsed times double.
 	w              int     // Visualization width, in pixels.
 	h              int     // Visualization height, in pixels.
-	colorSteps     int     // The number of color steps before saturation.
+	colors         int     // The number of color steps before saturation.
+	action         string  // Indication of action to be taken.
+	iPath          string  // Filesystem path for input.
+	oPath          string  // Filesystem path for output.
 )
-
-// Variables for fixed-position command-line arguments.
-var (
-	action string // Indication of what type of visualization to generate.
-	iPath  string // Filesystem path for input.
-	oPath  string // Filesystem path for output.
-)
-
-func convertCommaSeparatedToBinary() {
-	feeds.ConvertCSVToBinary(iPath, oPath, tA, tΩ, typeFilter, errorClassConf)
-}
-
-func generateErrorStackVisualization() {
-	v := perspective.NewErrorStack(w, h)
-	generateVisualization(v)
-}
-
-func generateHistogramVisualization() {
-	v := perspective.NewHistogram(w, h, yLog2)
-	generateVisualization(v)
-}
-
-func generateRollingStackVisualization() {
-	v := perspective.NewRollingStack(w, h, tA, tΩ)
-	generateVisualization(v)
-}
-
-func generateScatterVisualization() {
-	v := perspective.NewScatter(w, h, tΩ, tA, yLog2, colorSteps, xGrid)
-	generateVisualization(v)
-}
-
-func generateSortedWaveVisualization() {
-	v := perspective.NewSortedWave(w, h, tA, tΩ)
-	generateVisualization(v)
-}
-
-func generateStatusStackVisualization() {
-	v := perspective.NewStatusStack(w, h)
-	generateVisualization(v)
-}
-
-func generateSweepVisualization() {
-	v := perspective.NewSweep(w, h, tA, tΩ, yLog2, colorSteps, xGrid)
-	generateVisualization(v)
-}
-
-func generateWaveVisualization() {
-	v := perspective.NewWave(w, h, tA, tΩ)
-	generateVisualization(v)
-}
-
-func generateVisualization(v perspective.Visualizer) {
-	feeds.GeneratePNGFromBinLog(iPath, oPath, tA, tΩ, typeFilter, v)
-}
 
 func main() {
 
@@ -128,7 +75,7 @@ func main() {
 		"Height of the rendered graph, in pixels.")
 
 	flag.IntVar(
-		&colorSteps,
+		&colors,
 		"color-steps",
 		1,
 		"Number of color steps to use in rendering before clipping.")
@@ -145,25 +92,33 @@ func main() {
 	oPath = flag.Arg(2)
 
 	if action == "csv-convert" {
-		convertCommaSeparatedToBinary()
+		convertCSV()
 	} else if action == "vis-error-stack" {
-		generateErrorStackVisualization()
+		visualize(perspective.NewErrorStack(w, h))
 	} else if action == "vis-histogram" {
-		generateHistogramVisualization()
+		visualize(perspective.NewHistogram(w, h, yLog2))
 	} else if action == "vis-rolling-stack" {
-		generateRollingStackVisualization()
+		visualize(perspective.NewRollingStack(w, h, tA, tΩ))
 	} else if action == "vis-scatter" {
-		generateScatterVisualization()
+		visualize(perspective.NewScatter(w, h, tΩ, tA, yLog2, colors, xGrid))
 	} else if action == "vis-status-stack" {
-		generateStatusStackVisualization()
+		visualize(perspective.NewStatusStack(w, h))
 	} else if action == "vis-sweep" {
-		generateSweepVisualization()
+		visualize(perspective.NewSweep(w, h, tA, tΩ, yLog2, colors, xGrid))
 	} else if action == "vis-wave" {
-		generateWaveVisualization()
+		visualize(perspective.NewWave(w, h, tA, tΩ))
 	} else if action == "vis-wave-sorted" {
-		generateSortedWaveVisualization()
+		visualize(perspective.NewSortedWave(w, h, tA, tΩ))
 	} else {
 		log.Println("Unrecognized action.")
 		os.Exit(1)
 	}
+}
+
+func convertCSV() {
+	feeds.ConvertCSVToBinary(iPath, oPath, tA, tΩ, typeFilter, errorClassConf)
+}
+
+func visualize(v perspective.Visualizer) {
+	feeds.GeneratePNGFromBinLog(iPath, oPath, tA, tΩ, typeFilter, v)
 }
