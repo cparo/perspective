4a75b1a40ef44e639e97cfb4d38edd93e435f500
Christian Paro <cparo@digitalocean.com>
2014-10-22 03:05:08 -0400

    Remove redundant commentary on CLI flags
    Everything said in that commentary should be clear from either the
    variable-declaration comments or the surrounding context.

--- perspective_main.go
+++ perspective_main.go
@@ -12,7 +12,7 @@ import (
 // Variables for command-line option flags.
 var (
 	errorClassConf string  // Optional conf file for error classification.
-	typeFilter     int     // Event type code to filter for, if this is > 0.
+	typeFilter     int     // Event type code to filter for, if non-negative.
 	tA             int     // Lower limit of time range to be visualized.
 	tΩ             int     // Upper limit of time range to be visualized.
 	xGrid          int     // Number of horizontal grid divisions.
@@ -75,66 +75,54 @@ func generateWaveVisualization() {
 
 func main() {
 
-	// Default to no error reason coding. Applies only to the conversion of CSV
-	// files to binary log format. TODO: Document filter conf format.
 	flag.StringVar(
 		&errorClassConf,
 		"error-reason-filter",
 		"",
 		"Error reason filter congfiguration.")
 
-	// Default to showing all events.
 	flag.IntVar(
 		&typeFilter,
 		"event-type-id",
 		-1,
 		"Event type ID to filter for.")
 
-	// Default to the beginning of the epoch.
 	flag.IntVar(
 		&tA,
 		"min-time",
 		0,
 		"Least recent time to show, expressed as seconds in Unix epoch time.")
 
-	// Default to now.
 	flag.IntVar(
 		&tΩ,
 		"max-time",
 		int(time.Now().Unix()),
 		"Most recent time to show, expressed as seconds in Unix epoch time.")
 
-	// Default to no grid. No effect on conversion of CSV files to binary log
-	// format.
 	flag.IntVar(
 		&xGrid,
 		"x-grid",
 		0,
 		"Number of divisions to be separated with vertical grid lines.")
 
-	// Default to covering a power of two seconds run-time for every 16 pixels
-	// on the y axis. No effect on conversion of CSV files to binary log format.
 	flag.Float64Var(
 		&yLog2,
 		"run-time-scale",
 		16,
 		"Pixels along y-axis for every doubling in seconds of run time.")
 
-	// Default to 256 pixels. May not apply for all graph types.
 	flag.IntVar(
 		&w,
 		"width",
 		256,
 		"Width of the rendered graph, in pixels.")
 
-	// Default to 128 pixels. May not apply for all graph types.
 	flag.IntVar(
 		&h,
 		"height",
 		128,
 		"Height of the rendered graph, in pixels.")
 
-	// Default to just one. May not apply for all graph types.
 	flag.IntVar(
 		&colorSteps,
 		"color-steps",
