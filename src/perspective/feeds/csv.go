9041bf01d2b8f04de1813c23fced180a873fc14c
Christian Paro <cparo@digitalocean.com>
2014-10-22 03:09:01 -0400

    Reorganize helper functions in feeds package
    Move things which could be useful for multiple feed-processing steps or
    formats into a common.go file where they'll be more easily seen.

--- src/perspective/feeds/csv.go
+++ src/perspective/feeds/csv.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"encoding/binary"
 	"encoding/csv"
-	"io"
 	"log"
 	"os"
 	"regexp"
@@ -141,53 +140,6 @@ func ConvertCSVToBinary(
 	exitOnError(
 		binWriter.Flush(),
 		"Error flushing data to binary log.")
-}
-
-func eventFilter(
-	startTime int,
-	eventType int,
-	minTime int,
-	maxTime int,
-	typeFilter int) bool {
-	if minTime < startTime && maxTime > startTime {
-		if typeFilter < 0 || eventType == typeFilter {
-			return true
-		}
-	}
-	return false
-}
-
-func atEOF(err error, message string) bool {
-	if err != nil {
-		if err == io.EOF {
-			return true
-		}
-		log.Println(message)
-		log.Println(err)
-		os.Exit(1)
-	}
-	return false
-}
-
-func exitOnError(err error, message string) {
-	if err != nil {
-		log.Println(message)
-		log.Println(err)
-		os.Exit(1)
-	}
-}
-
-func openFiles(iPath string, oPath string) (iFile *os.File, oFile *os.File) {
-
-	var err error
-
-	iFile, err = os.Open(iPath)
-	exitOnError(err, "Failed to open input file for reading.")
-
-	oFile, err = os.Create(oPath)
-	exitOnError(err, "Failed to open output file for writing.")
-
-	return iFile, oFile
 }
 
 func getErrorCode(errorReason string, errorFilters []*regexp.Regexp) int16 {
