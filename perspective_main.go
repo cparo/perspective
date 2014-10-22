9f4615350b06800dead3ad5041a4ff46376dd263
Christian Paro <cparo@digitalocean.com>
2014-10-22 01:23:22 -0400

    Remove references to Alpha
    ...what is this "Alpha" thing anyway? Let's just call it a "reference
    data source", since that's what matters about its role relative to the
    reference implementation for calling this visualization library.

--- perspective_main.go
+++ perspective_main.go
@@ -25,10 +25,10 @@ import (
 // performance, but it is pretty convenient for reading a hex dump of the
 // resulting binary event log format.
 type eventData struct {
-	EventID   int32 // Event ID as recorded in Alpha.
+	EventID   int32 // Event ID as recorded in reference data source.
 	StartTime int32 // In seconds since the beginning of the Unix epoch.
 	RunTime   int32 // Event run time, in seconds.
-	EventType int16 // Event type ID as recorded in Alpha.
+	EventType int16 // Event type ID as recorded in reference data source.
 	Status    int16 // Zero indicates success, non-zero indicates failure.
 }
 
